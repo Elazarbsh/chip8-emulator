47f86d1c7e1e43af8094223c02025ef62624c7a0
Elazar <elazarbsh@gmail.com>
2023-03-06 17:28:40 +0200

    add tests for the stack and register file

---
+++ tests/register-file.test.js
@@ -0,0 +1,81 @@
+import { beforeEach, it, expect, describe } from "vitest";
+import { RegisterFile } from "../src/register-file";
+
+let rf;
+
+beforeEach(() => {
+    rf = new RegisterFile(16);
+});
+
+describe('getRegister', () => {
+    it("should throw an error if the register index is out of range for getRegister", () => {
+        const rf = new RegisterFile(4);
+        expect(() => rf.getRegister(5)).toThrow();
+    });
+});
+
+
+describe('setRegister', () => {
+    it("should set the value of the specified register", () => {
+        rf.setRegister(0, 10)
+        expect(rf.getRegister(0)).toBe(10);
+    });
+
+    it("should throw an error if the register index is out of range for setRegister", () => {
+        const rf = new RegisterFile(4);
+        expect(() => rf.setRegister(5, 10)).toThrow();
+    });
+});
+
+describe('addToReg', () => {
+    it("should add the value to the specified register", () => {
+        rf.setRegister(2, 20);
+        rf.addToReg(2, 5);
+        expect(rf.getRegister(2)).toBe(25);
+    });
+
+    it("should throw an error if the register index is out of range for addToReg", () => {
+        const rf = new RegisterFile(4);
+        expect(() => rf.addToReg(5, 5)).toThrow();
+    });
+});
+
+describe('copyReg', () => {
+    it("should copy the value from the source register to the target register", () => {
+        rf.setRegister(0, 2);
+        rf.copyReg(1, 0);
+        expect(rf.getRegister(1)).toBe(2);
+    });
+
+    it("should throw an error if the register index is out of range for copyReg target", () => {
+        const rf = new RegisterFile(4);
+        expect(() => rf.copyReg(5, 0)).toThrow();
+    });
+
+    it("should throw an error if the register index is out of range for copyReg source", () => {
+        const rf = new RegisterFile(4);
+        expect(() => rf.copyReg(0, 5)).toThrow();
+    });
+});
+
+describe('reset', () => {
+    it("should set all registers to 0", () => {
+        const rf = new RegisterFile(3);
+        rf.setRegister(0, 10);
+        rf.setRegister(1, 20);
+        rf.setRegister(2, 30);
+        rf.reset();
+        expect(rf.getRegister(0)).toBe(0);
+        expect(rf.getRegister(1)).toBe(0);
+        expect(rf.getRegister(2)).toBe(0);
+    });
+});
+
+
+
+
+
+
+
+
+
---
+++ tests/stack.test.js
@@ -0,0 +1,54 @@
+import { describe, expect, it } from "vitest";
+import { Stack } from "../src/stack.js";
+
+
+describe('push', () => {
+    it("should push to the stack", () => {
+        const stack = new Stack(1);
+        stack.push(1);
+        const result = stack.pop();
+        expect(result).toBe(1);
+    });
+
+    it('should throw an error if the stack is full', () => {
+        const stack = new Stack(1);
+        stack.push(1);
+        expect(() => stack.push(2)).toThrow();
+    });
+});
+
+describe('pop', () => {
+    it('should remove and return the last value added to the stack', () => {
+        const stack = new Stack(2);
+        stack.push(1);
+        stack.push(2);
+        expect(stack.pop()).toEqual(2);
+        expect(stack.pop()).toEqual(1);
+    });
+
+    it('should throw an error when the stack is empty', () => {
+        const stack = new Stack(1);
+        expect(() => stack.pop()).toThrow();
+    });
+
+});
+
+describe('reset', () => {
+    it('should reset the stack to its initial state', () => {
+        const stack = new Stack(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.reset();
+        expect(stack.pop()).toBeUndefined();
+    });
+});
+
+
+
+
+
+
+
+
+
